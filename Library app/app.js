64b9024cdd961bf75326be12554ddf3e0212edeb
jesna <jasna.edava@gmail.com>
2021-04-16 19:07:09 +0530

    first commit:first version

--- Library app/app.js
+++ Library app/app.js
@@ -1,28 +1,35 @@
-const express = require("express");
+const express = require('express');
+const { request } = require('express');
+const authRouter = express.Router();
 const app = new express();
-
-const port = process.env.PORT ||5000;
-
-const booksrouter = require('./src/routes/bookroutes');
-const authorsrouter = require('./src/routes/authorroutes');
-
-
-
+var books = require('./books');
+var authors = require('./authors');
+module.exports = app;
 app.use(express.static('./public'));
-
-app.set('view engine','ejs');
-app.set('views','./src/views');
-app.use('/books',booksrouter);
-app.use('/authors',authorsrouter);
-
-
-app.get('/',function(req,res){
-    res.render("index",
-    {
-      nav:[{link:'/books',name:'Books'},{link:'/authors',name:'Authors'},{link:'/login',name:'Login'},{link:'/signup',name:'Signup'}],
-      title:'Library'
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/src/views');
+app.use('/', authRouter);
+app.get('/', function(req, res) {
+    res.render("index", {
+        nav: [{ link: '/', name: 'Home' }, { link: '/books', name: 'Books' }, { link: '/authors', name: 'Authors' }, { link: '/sign-in', name: 'Sign In' }, { link: '/sign-up', name: 'Sign Up' }],
+        title: 'Library App'
     });
 });
-
-
-app.listen(port,()=>{console.log("Server Ready at"+port)});
+authRouter.get('/sign-in', function(req, res) {
+    res.render("sign-in", {
+        nav: [{ link: '/', name: 'Home' }, { link: '/books', name: 'Books' }, { link: '/authors', name: 'Authors' }, { link: '/sign-in', name: 'Sign In' }, { link: '/sign-up', name: 'Sign Up' }],
+        title: 'Library App'
+    });
+});
+authRouter.get('/sign-up', function(req, res) {
+    res.render("sign-up", {
+        nav: [{ link: '/', name: 'Home' }, { link: '/books', name: 'Books' }, { link: '/authors', name: 'Authors' }, { link: '/sign-in', name: 'Sign In' }, { link: '/sign-up', name: 'Sign Up' }],
+        title: 'Library App'
+    });
+});
+app.get('/authors', authors.list);
+app.get('/authors/:id', authors.view);
+app.get('/books', books.list);
+app.get('/books/:id', books.view);
+app.get('/add-book', books.new);
+app.listen(5000);
